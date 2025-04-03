e7f61c4103a435a3403ab786558e85fedcd9e9ea
Daniel Gilleran <dangilleran7@protonmail.com>
2025-04-03 01:37:14 -0400

    got basic chatbox working. next goal is getting ssh server to work

--- main.go
+++ main.go
@@ -1,69 +1,196 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
 
-	"github.com/charmbracelet/charm"
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+
+	//SSH server
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/bubbletea"
+	"github.com/charmbracelet/wish/logging"
 )
 
-type Chatroom struct {
-	clients     map[net.Addr]charm.Client
-	messageChan chan string
+const (
+	host = "localhost"
+	port = "8080"
+	gap  = "\n\n"
+)
+
+type (
+	errMsg error
+)
+
+// contains all state
+type model struct {
+	viewport    viewport.Model
+	messages    []string
+	textarea    textarea.Model
+	senderStyle lipgloss.Style
+	err         error
+}
+
+func initialModel() model {
+
+	ta := textarea.New()
+	ta.Placeholder = "Send a message..."
+	ta.Focus()
+
+	ta.Prompt = "┃ "
+	ta.CharLimit = 1000
+
+	ta.SetWidth(30)
+	ta.SetHeight(3)
+
+	// Remove cursor line styling
+	ta.FocusedStyle.CursorLine = lipgloss.NewStyle()
+
+	ta.ShowLineNumbers = false
+
+	vp := viewport.New(30, 5)
+	vp.SetContent(`Welcome to the chat room!
+Type a message and press Enter to send.`)
+
+	ta.KeyMap.InsertNewline.SetEnabled(false)
+
+	return model{
+		textarea:    ta,
+		messages:    []string{},
+		viewport:    vp,
+		senderStyle: lipgloss.NewStyle().Foreground(lipgloss.Color("5")),
+		err:         nil,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+
+	var (
+		tiCmd tea.Cmd
+		vpCmd tea.Cmd
+	)
+
+	m.textarea, tiCmd = m.textarea.Update(msg)
+	m.viewport, vpCmd = m.viewport.Update(msg)
+
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.viewport.Width = msg.Width
+		m.textarea.SetWidth(msg.Width)
+		m.viewport.Height = msg.Height - m.textarea.Height() - lipgloss.Height(gap)
+
+		if len(m.messages) > 0 {
+			// Wrap content before setting it.
+			m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
+		}
+		m.viewport.GotoBottom()
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			fmt.Println(m.textarea.Value())
+			return m, tea.Quit
+		case tea.KeyEnter:
+			m.messages = append(m.messages, m.senderStyle.Render("You: ")+m.textarea.Value()) //TODO: Replace 'You: ' with assigned number
+			m.viewport.SetContent(lipgloss.NewStyle().Width(m.viewport.Width).Render(strings.Join(m.messages, "\n")))
+			m.textarea.Reset()
+			m.viewport.GotoBottom()
+		}
+
+	// We handle errors just like any other message
+	case errMsg:
+		m.err = msg
+		return m, nil
+	}
+
+	return m, tea.Batch(tiCmd, vpCmd)
+}
+
+func (m model) View() string {
+	// The header
+	s := "Le Chat 0.1\n\n"
+
+	// for _, user := range m.users {
+	// 	s += fmt.Sprintf("[%s]: ", user)
+	// }
+
+	// for _, message := range m.messages {
+
+	// 	// Render the row
+	// 	s += fmt.Sprintf("%s\n", message)
+	// }
+
+	// // The footer
+	// s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	s += fmt.Sprintf(
+		"%s%s%s",
+		m.viewport.View(),
+		gap,
+		m.textarea.View(),
+	)
+
+	return s
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:2222")
+
+	s, err := wish.NewServer(
+		wish.WithAddress(net.JoinHostPort(host, port)),
+		wish.WithHostKeyPath(".ssh/id_ed25519"),
+		wish.WithMiddleware(
+			bubbletea.Middleware(teaHandler),
+			// activeterm.Middleware(), // Bubble Tea apps usually require a PTY.
+			logging.Middleware(),
+		),
+	)
 	if err != nil {
-		log.Fatalf("Failed to listen on port 2222: %v", err)
+		log.Error("Could not start server", "error", err)
 	}
-
-	fmt.Println("Chatroom server is running on port 2222...")
-
-	chatroom := &Chatroom{
-		clients:     make(map[net.Addr]charm.Client),
-		messageChan: make(chan string),
-	}
-
-	go chatroom.broadcastMessages()
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Fatalf("Failed to accept incoming connection: %v", err)
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	log.Info("Starting SSH server", "host", host, "port", port)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
 		}
-
-		client := charm.NewClient(conn, charm.DefaultTerminalSize)
-		chatroom.clients[conn.RemoteAddr()] = client
-
-		go chatroom.handleConnection(conn, client)
-	}
-}
-
-func (c *Chatroom) handleConnection(conn net.Conn, client charm.Client) {
-	defer func() {
-		conn.Close()
-		delete(c.clients, conn.RemoteAddr())
 	}()
 
-	client.Println("Welcome to the chatroom!")
-
-	for {
-		message, err := client.ReadLine()
-		if err != nil {
-			log.Printf("Failed to read from client: %v", err)
-			return
-		}
-
-		c.messageChan <- message
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
 	}
-}
 
-func (c *Chatroom) broadcastMessages() {
-	for message := range c.messageChan {
-		for _, client := range c.clients {
-			client.Println(message)
-		}
-	}
+	// p := tea.NewProgram(initialModel())
+
+	// if _, err := p.Run(); err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// p := tea.NewProgram(initialModel())
+	// if _, err := p.Run(); err != nil {
+	// 	fmt.Printf("Alas, there's been an error: %v", err)
+	// 	os.Exit(1)
+	// }
+
 }
